17f04c2083f1db33e40a2a578d36f0dd67706e03
Aidyn <adilkhan.a@rahmetapp.kz>
2021-12-06 22:04:09 +0600

    added first simple test

---
+++ src/test/java/biz/rahmet/gitexample/SimpleClass.java
@@ -0,0 +1,12 @@
+package biz.rahmet.gitexample;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleClass {
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(true);
+    }
+}
